b1d8818a9f2468239f019365dfe448052490ece1
Ben Lapuhapo <ben.lapuhapo@mths.ca>
2019-09-12 17:11:14 +0000

    Added rectangle calculator program

---
+++ rectangle_calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by: Ben Lapuhapo
+# Created on: September 12 2019
+# This program calculates area and perimeter of a rectangle
+
+
+def main():
+    print("The Rectangle's dimensions are L= 5, W= 3")
+    print("The rectangle's perimeter is {}".format(2*5+2*3))
+    print("The rectangle's area is {}".format(5*3))
+
+
+if __name__ == "__main__":
+    main()
